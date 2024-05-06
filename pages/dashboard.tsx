14fe13533c8ebb4bca494d6ba4862bad5cb74916
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2024-05-06 12:09:15 +0100

    Wip

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Tab, Tabs } from '@mui/material';
+import { Box, Button, TextField, Tab, Tabs, Container } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -34,59 +34,101 @@ const DashboardContent: React.FC = () => {
     };
 
     return (
-        <>
-            <Button type="submit" onClick={handleUpdateAccountDetails}>Update Account Details</Button>
-            <TextField
-                variant="outlined"
-                required
-                id="filled-required-first-name"
-                label="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-            />
-            <TextField
-                variant="outlined"
-                required
-                id="filled-required-last-name"
-                label="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-            <TextField
-                variant="outlined"
-                required
-                id="filled-required-account-number"
-                label="Account Number"
-                value={accountNumber}
-                onChange={(e) => setAccountNumber(e.target.value)}
-            />
-            <TextField
-                variant="outlined"
-                required
-                id="filled-required-deposit"
-                label="Deposit"
-                value={deposit}
-                onChange={(e) => setDeposit(Number(e.target.value))}
-            />
-            <Button type="submit" onClick={handleDeposit}>Deposit</Button>
-            <TextField
-                variant="outlined"
-                required
-                id="filled-required-withdraw"
-                label="Withdraw"
-                value={withdraw}
-                onChange={(e) => setWithdraw(Number(e.target.value))}
-            />
-            <Button type="submit" onClick={handleWithdraw}>Withdraw</Button>
-        </>
+        <Container
+            maxWidth="xl"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                flexGrow: 1,
+                height: '100vh',
+                mt: { xs: 0, sm: 12 }, // Remove top margin on extra-small screens
+                overflow: 'hidden',
+            }}
+        >
+            <Box
+                component="div"
+            >  <Box>
+                    <Box>
+                        <Button type="submit" onClick={handleUpdateAccountDetails} sx={{ mt: 2 }}>Update Account Details</Button>
+                    </Box>
+                    <TextField
+                        variant="outlined"
+                        required
+                        id="filled-required-first-name"
+                        label="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </Box>
+                <Box>
+                    <TextField
+                        variant="outlined"
+                        required
+                        id="filled-required-last-name"
+                        label="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Box>
+                <Box>
+                    <TextField
+                        variant="outlined"
+                        required
+                        id="filled-required-account-number"
+                        label="Account Number"
+                        value={accountNumber}
+                        onChange={(e) => setAccountNumber(e.target.value)}
+                    />
+                </Box>
+            </Box>
+            <Box>
+                <Box>
+                    <Button type="submit" onClick={handleDeposit} sx={{ mt: 2 }}>Give Loan</Button>
+                </Box>
+                <TextField
+                    variant="outlined"
+                    required
+                    id="filled-required-deposit"
+                    label="Deposit"
+                    value={deposit}
+                    onChange={(e) => setDeposit(Number(e.target.value))}
+                />
+            </Box>
+
+
+            <Box>
+                <Box>
+                    <Button type="submit" onClick={handleWithdraw} sx={{ mt: 2 }}>Receive Loan</Button>
+                </Box>
+                <TextField
+                    variant="outlined"
+                    required
+                    id="filled-required-withdraw"
+                    label="Withdraw"
+                    value={withdraw}
+                    onChange={(e) => setWithdraw(Number(e.target.value))}
+                />
+
+            </Box>
+        </Container>
     );
 }
 
 const ProfileContent: React.FC = () => {
     return (
-        <>
+        <Container
+            maxWidth="xl"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                flexGrow: 1,
+                height: '100vh',
+                mt: { xs: 0, sm: 12 }, // Remove top margin on extra-small screens
+                overflow: 'hidden',
+            }}
+        >
             {/* Profile content goes here */}
-        </>
+        </Container>
     );
 }
 
@@ -105,17 +147,7 @@ const Dashboard: React.FC = () => {
     };
 
     return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': {
-                    m: 1,
-                    width: { xs: '100%', sm: '50ch' } // 90% width on small screens, 50ch on larger screens
-                },
-            }}
-            noValidate
-            autoComplete="off"
-        >
+        <>
             <h1>Dashboard</h1>
             <Tabs value={tabValue} onChange={handleTabChange}>
                 <Tab label="Dashboard" />
@@ -123,7 +155,9 @@ const Dashboard: React.FC = () => {
             </Tabs>
             {tabValue === 0 && <DashboardContent />}
             {tabValue === 1 && <ProfileContent />}
-        </Box>
+
+        </>
+
     );
 }
 
